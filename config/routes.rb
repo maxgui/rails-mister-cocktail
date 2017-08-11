4dee3c02d8dab8eed9019db679970703dd5096ec
Maxime Guillemain <maxguillemain@gmail.com>
2017-08-11 17:35:36 +0200

    mister cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Attachinary::Engine => "/attachinary"
 
   resources :cocktails, only: [:index, :create, :new, :show] do
     resources :doses, only: [:create]
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,33 @@ require 'json'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
 Ingredient.destroy_all
+Cocktail.destroy_all
+
 
 puts 'cleaning db'
 
-url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+url_in = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 
-ingredient_url = open(url).read
+ingredient_url = open(url_in).read
 ingredients = JSON.parse(ingredient_url)
 
 ingredients["drinks"].each do |ingredient|
-  Ingredient.create(name: ingredient["strIngredient1"])
+  Ingredient.create!(name: ingredient["strIngredient1"])
 end
 
+url_co = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+
+cocktail_url = open(url_co).read
+cocktails = JSON.parse(cocktail_url)
+
+cocktails["drinks"].first(10).each do |cocktail|
+  Cocktail.create!(name: cocktail["strDrink"])
+end
+
+cocktails["drinks"].first(10).each do |cocktail|
+  Cocktail.create!(picture: cocktail["strDrinkThumb"])
+end
+
+puts "done"
